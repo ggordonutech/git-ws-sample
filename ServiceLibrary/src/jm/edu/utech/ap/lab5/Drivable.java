26941fb99a3af4db9fd579eb81532ee2b6ac70c8
gggordon <ggg_gilroy@yahoo.com>
2018-09-27 08:33:06 -0400

    Added Drivable interface

---
+++ ServiceLibrary/src/jm/edu/utech/ap/lab5/Drivable.java
@@ -0,0 +1,12 @@
+package jm.edu.utech.ap.lab5;
+
+/**
+ * Indicates that the type may be driven
+ * */
+public interface Drivable {
+	/**
+	 * Implement driving behaviour
+	 * */
+	void drive();
+
+}
