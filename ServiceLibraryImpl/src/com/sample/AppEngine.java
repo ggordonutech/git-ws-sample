2c46614be4d545cbabb258bfc84531743d02bce5
gggordon <ggg_gilroy@yahoo.com>
2018-09-27 08:46:03 -0400

    Added Service Implementation with runnable main in AppEngine

---
+++ ServiceLibraryImpl/src/com/sample/AppEngine.java
@@ -0,0 +1,13 @@
+package com.sample;
+
+import jm.edu.utech.ap.lab5.Drivable;
+
+public class AppEngine {
+
+	public static void main(String[] args) {
+		Drivable driver = new Driver();
+		driver.drive();
+
+	}
+
+}
---
+++ ServiceLibraryImpl/src/com/sample/Driver.java
@@ -0,0 +1,11 @@
+package com.sample;
+
+import jm.edu.utech.ap.lab5.Drivable;
+
+public class Driver implements Drivable{
+	
+	public void drive() {
+		System.out.println("This driver can drive!");
+	}
+
+}
